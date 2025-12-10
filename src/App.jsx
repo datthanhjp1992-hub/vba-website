c0d0ae6f0aac5afa166d5be997563feccaf71274
datthanhjp1992-hub <dat.thanhjp1992@gmail.com>
2025-12-10 16:51:54 +0900

    Refactor App component to include layout panels

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import TopPanel from './components/panel_top';
+import BottomPanel from './components/panel_bottom';
+import LeftPanel from './components/panel_left';
+import CenterPanel from './components/panel_center';
+import RightPanel from './components/panel_right';
 import './css/App.css';
 
 function App() {
   return (
-    <div> This is a test version</div>
+    <div className="app">
+      <TopPanel />
+      
+      <div className="main-container">
+        <LeftPanel />
+        <CenterPanel />
+        <RightPanel />
+      </div>
+      
+      <BottomPanel />
+    </div>
   );
 }
 
