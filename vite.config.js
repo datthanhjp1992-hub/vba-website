8211074e1c27a14e3e53f71795175ccc77a4e308
datthanhjp1992-hub <dat.thanhjp1992@gmail.com>
2025-12-10 17:01:01 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/vba-website/' : '/',
   plugins: [react()], 
-  esbuild: {
-    loader: 'jsx',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 })
