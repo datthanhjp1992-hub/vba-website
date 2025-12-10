13beca8275396a25d14189598aa07b26301c9931
datthanhjp1992-hub <dat.thanhjp1992@gmail.com>
2025-12-10 16:27:17 +0900

    fix 404

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/vba-website/",
+  base: process.env.NODE_ENV === 'production' ? '/vba-website/' : '/',
   plugins: [react()], 
 })
