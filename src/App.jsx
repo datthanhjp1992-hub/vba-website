2644092ff85b5402add06d5be606a2b20ce5e4f0
datthanhjp1992-hub <dat.thanhjp1992@gmail.com>
2025-12-10 16:49:31 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import TopPanel from './components/panel_top';
-import BottomPanel from './components/panel_bottom';
-import LeftPanel from './components/panel_left';
-import CenterPanel from './components/panel_center';
-import RightPanel from './components/panel_right';
 import './css/App.css';
 
 function App() {
   return (
-    <div className="app">
-      <TopPanel />
-      
-      <div className="main-container">
-        <LeftPanel />
-        <CenterPanel />
-        <RightPanel />
-      </div>
-      
-      <BottomPanel />
-    </div>
+    <div> This is a test version</div>
   );
 }
 
