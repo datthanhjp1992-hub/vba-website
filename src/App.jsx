9ce9798670fd798e103abc1532af94653f1f106a
datthanhjp1992-hub <dat.thanhjp1992@gmail.com>
2025-12-10 15:50:22 +0900

    change css directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './css/App.css'
+import React from 'react';
+import TopPanel from './components/TopPanel';
+import BottomPanel from './components/BottomPanel';
+import LeftPanel from './components/LeftPanel';
+import CenterPanel from './components/CenterPanel';
+import RightPanel from './components/RightPanel';
+import './css/App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <TopPanel />
+      
+      <div className="main-container">
+        <LeftPanel />
+        <CenterPanel />
+        <RightPanel />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+      <BottomPanel />
+    </div>
+  );
 }
 
-export default App
+export default App;
